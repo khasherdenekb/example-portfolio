01b30568a7ef76f67cfe06dff298483d61da0909
khasherdene.b <khasherdene.kb@gmail.com>
2024-05-05 18:08:36 +0800

    added video and images

--- src/app/videos/page.tsx
+++ src/app/videos/page.tsx
@@ -1,18 +1,40 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import React from "react";
 
 const Videos = () => {
   return (
-    <div className="py-8 !z-[1]">
-      <div className=" relative w-full pb-[56.25%]">
-        <iframe
-          className="absolute top-0 left-0 w-full h-full"
-          src="https://www.youtube.com/embed/Sf0XrkvSx0A?autoplay=1"
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen
-        ></iframe>
-      </div>
+    <div className="py-5">
+      <Card>
+        <CardHeader>
+          <CardTitle>
+            <h2 className="scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0 pb-2 text-[#266431]">
+              Бидний талаарх контент
+            </h2>
+          </CardTitle>
+          <CardDescription>
+            <p>Та бидний талаарх контентуудыг нэг дороос харах боломжтой.</p>
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div>
+            <div className="h-[650px]">
+              <iframe
+                className="flex-1 w-full h-full"
+                src="https://www.youtube.com/embed/wCYR2tUQlaw?si=IB3_truxFna2OJXn"
+                frameBorder="0"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+              />
+            </div>
+          </div>
+        </CardContent>
+      </Card>
     </div>
   );
 };
--- src/components/shared/hero.tsx
+++ src/components/shared/hero.tsx
@@ -1,60 +1,35 @@
-"use client";
 import React from "react";
-import { motion } from "framer-motion";
-import dynamic from "next/dynamic";
-import { ArrowUpRight } from "lucide-react";
 import { Button } from "../ui/button";
-import { globeConfig, sampleArcs } from "@/data/dummyData";
-
-const World = dynamic(
-  () => import("@/components/ui/globe").then((m) => m.World),
-  {
-    ssr: false,
-  }
-);
+import { ArrowUpRight } from "lucide-react";
 
 const Hero = () => {
   return (
-    <div className="relative z-[-1]">
-      <div className="flex flex-row items-center justify-center py-20 h-screen md:h-auto dark:bg-black bg-white relative w-full">
-        <div className="mx-auto w-full relative overflow-hidden h-full md:h-[40rem] px-4">
-          <motion.div
-            initial={{
-              opacity: 0,
-              y: 20,
-            }}
-            animate={{
-              opacity: 1,
-              y: 0,
-            }}
-            transition={{
-              duration: 1,
-            }}
-            className="div"
-          >
-            <h2 className="text-center text-xl md:text-4xl font-bold text-[#224945]">
-              Шинэ аялал
-            </h2>
-            <p className="text-center text-base md:text-lg font-normal cursor-pointer max-w-md mt-2 mx-auto text-[#266431] !z-[1]">
-              Our North American Field Guides provide tips for identifying birds
-              in all of the regions of the United States and Canada. Download
-              any of our guides for free now!{" "}
-              <Button className="bg-[#2A6F37]">
-                <p>Үзэх</p>
-                <ArrowUpRight className="pl-2" />
-              </Button>
-            </p>
-          </motion.div>
-          <div className="absolute w-full bottom-0 inset-x-0 h-40 bg-gradient-to-b pointer-events-none select-none from-transparent dark:to-black to-white z-40" />
-          <div
-            onClick={() => console.log("LOGIC GOES HERE")}
-            className="absolute w-full -bottom-20 h-72 md:h-full z-[1] cursor-pointer"
-          >
-            <World data={sampleArcs} globeConfig={globeConfig} />;
+    <section>
+      <div className="mx-auto px-4 py-32 lg:flex lg:h-[700px] lg:items-center">
+        <div className="mx-auto max-w-xl text-center">
+          <h1 className="text-3xl font-extrabold sm:text-5xl">
+            Тавтай морилно уу
+            <strong className="font-extrabold text-green-700 sm:block">
+              {" "}
+              Шинэ Аялал{" "}
+            </strong>
+          </h1>
+
+          <p className="mt-4 sm:text-xl/relaxed">
+            Our North American Field Guides provide tips for identifying birds
+            in all of the regions of the United States and Canada. Download any
+            of our guides for free now!
+          </p>
+
+          <div className="mt-8 flex flex-wrap justify-center gap-4">
+            <Button className="bg-[#2A6F37] w-96" size={"lg"}>
+              Үзэх
+              <ArrowUpRight className="pl-2" />
+            </Button>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
