ba3b45bb5ffb226532b7a8e29c5ac9683362d164
khasherdene.b <khasherdene.kb@gmail.com>
2024-06-01 13:53:50 +0800

    fix correct spelling

--- src/app/business-journey/layout.tsx
+++ src/app/business-journey/layout.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
   openGraph: {
     images: [
       {
-        url: "/travel-banner1.jgg",
+        url: "/travel-banner1.jpg",
         width: 1200,
         height: 630,
         alt: "travel-banner1",
