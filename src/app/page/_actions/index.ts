220e89fc2c62424c03b6ec7cbaa4c5a3350cdc4c
enkhbold.t <enkhbold0819@gmail.com>
2024-06-11 00:12:02 +0800

    added changes

--- src/app/page/_actions/index.ts
+++ src/app/page/_actions/index.ts
@@ -1,22 +1,25 @@
 import { API_URL, BASIC_AUTH_PW, BASIC_AUTH_USER } from "@/lib/constants";
 import useSWR from "swr";
 
-const fetcher = async (url: string) => {
+const fetcher = async (url: string, slug: string) => {
   const token = Buffer.from(`${BASIC_AUTH_USER}:${BASIC_AUTH_PW}`).toString(
     "base64"
   );
+  console.log(slug, "slug");
   return await fetch(url, {
     method: "POST",
     headers: {
       Authorization: `Basic ${token}`,
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ slug: "about" }),
+    body: JSON.stringify({ slug }),
   }).then((res) => res.json());
 };
 
-export function getAbout() {
-  const { data, error } = useSWR(`${API_URL}/api/v1/menu/getPages`, fetcher);
+export function getAbout(slug: string) {
+  const { data, error } = useSWR(`${API_URL}/api/v1/menu/getPages`, (url) =>
+    fetcher(url, slug)
+  );
 
   return {
     data: data?.response || {},
