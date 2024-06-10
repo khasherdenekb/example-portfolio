a17d32ccd71bac64622d01ec94d22ee0408d47fa
enkhbold.t <enkhbold0819@gmail.com>
2024-06-10 23:24:57 +0800

    added

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,16 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'admin.baaskaherbalifeclub.com',
+        port: '',
+        pathname: '/storage/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
