0f81fca6506e0d4abcf35cbaa9257ae91dd0fa9c
Khash-Erdene Bolor-Erdene <khasherdene@Khash-Erdenes-MacBook-Air.local>
2024-06-15 16:45:58 +0800

    remove unnecessary images

---
+++ src/app/gallery/_actions/index.ts
@@ -0,0 +1,30 @@
+import { API_URL, BASIC_AUTH_PW, BASIC_AUTH_USER } from "@/lib/constants";
+import useSWR from "swr";
+
+const fetcher = async (url: string) => {
+  const token = Buffer.from(`${BASIC_AUTH_USER}:${BASIC_AUTH_PW}`).toString(
+    "base64"
+  );
+  return await fetch(url, {
+    method: "POST",
+    headers: {
+      Authorization: `Basic ${token}`,
+      "Content-Type": "application/json",
+    },
+  }).then((res) => res.json());
+};
+
+export function getGalleries() {
+  const { data, error, isLoading } = useSWR(
+    `${API_URL}/api/category/getList`,
+    fetcher
+  );
+
+  console.log(data);
+
+  return {
+    data: data?.response || [],
+    isLoading,
+    isError: error,
+  };
+}
